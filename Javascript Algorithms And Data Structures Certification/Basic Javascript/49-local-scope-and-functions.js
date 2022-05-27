5f1ae89235052601091336be174c40fe5209cb67
Aby35-Game <106401343+Aby35-Game@users.noreply.github.com>
2022-05-27 19:37:19 +0200

    Update 49-local-scope-and-functions.js

--- Javascript Algorithms And Data Structures Certification/Basic Javascript/49-local-scope-and-functions.js
+++ Javascript Algorithms And Data Structures Certification/Basic Javascript/49-local-scope-and-functions.js
@@ -1,14 +1,11 @@
-  var myVar;
 function myLocalScope() {
-  'use strict'; // you shouldn't need to edit this line
+
+  // Only change code below this line
   var myVar;
-  console.log(myVar);
+  console.log('inside myLocalScope', myVar);
 }
 myLocalScope();
 
 // Run and check the console
 // myVar is not defined outside of myLocalScope
-console.log(myVar);
-
-// Now remove the console log line to pass the test
-
+console.log('outside myLocalScope', myVar);
